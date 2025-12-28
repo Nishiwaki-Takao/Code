64b1ab9b498b0967fdac5e408fa5ab3e91edefc7
西脇高雄 <garrusarcanjel@gmail.com>
2025-12-28 12:25:52 +0900

    Resistryを登録して202012validatorで再テスト

--- JISX0401/prefecture_schema.py
+++ JISX0401/prefecture_schema.py
@@ -5,7 +5,8 @@
 # 2020/08/08
 
 import json ,sys
-from jsonschema import validate, ValidationError, FormatChecker, Draft2020Validator, RefResolver
+from jsonschema import  ValidationError, FormatChecker, Draft202012Validator
+from referencing import Registry, Resource
 
 with open('Prefecture_schema.json', encoding="utf-8") as file_schema:
     json_schema = json.load(file_schema)
@@ -13,13 +14,22 @@ with open('Prefecture_schema.json', encoding="utf-8") as file_schema:
 with open('Prefecture_list.json', encoding="utf-8") as file_json:
     json_data = json.load(file_json)
 
-resolver = RefResolver(
-    base_uri=f"file://{BASE.as_posix()}/",
-    referrer=schema,
-)
+registry = Registry().with_resources({
+    # LocalPublicEntityCode.json
+    f"file://{('common\\LocalPublicEntityCode.json').as_posix()}":
+        Resource.from_contents(
+            json.loads((COMMON / 'LocalPublicEntityCode.json').read_text(encoding="utf-8"))
+        ),
+
+    # LocalPublicEntityCommonProperties.json
+    f"file://{('common\\LocalPublicEntityCommonProperties.json').as_posix()}":
+        Resource.from_contents(
+            json.loads((COMMON / 'LocalPublicEntityCommonProperties.json').read_text(encoding="utf-8"))
+        ),
+})
 
 try:
-    Draft2020Validator(json_data, json_schema,resolver=resolver, format_checker=FormatChecker())
+    Draft202012Validator(json_data, json_schema,registry=registry, format_checker=FormatChecker())
 except ValidationError as e:
     print(e.message)
     sys.exit(1)
